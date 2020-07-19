af9caf219ea7b060a3ce0383192c82cf326725ab
enjoys13 <joy.salomo@gmail.com>
2020-07-19 17:34:16 +0700

    initial commit

---
+++ src/main/java/co/id/jss/jssrestapi/JssRestApiApplication.java
@@ -0,0 +1,13 @@
+package co.id.jss.jssrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JssRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JssRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/jss/jssrestapi/JssRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.jss.jssrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JssRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
